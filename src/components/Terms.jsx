067e352011f1fcb6dcf3c025e3c46dc04f643a93
darudaniel <habladaru@gmail.com>
2023-12-04 20:34:38 -0500

    terms template

---
+++ src/components/Terms.jsx
@@ -0,0 +1,47 @@
+const Terms = () => {
+  return (
+    <div className="Terms">
+      <p>
+        Terms and Conditions of Use for the Financial Price Monitoring Application:
+
+        Please carefully read the following terms and conditions before using the financial price monitoring application ("Market Oberver"). By accessing and using the Application, you agree to be bound by these terms and conditions.
+
+        1. Permitted Use:
+
+        The Application is solely intended to provide users with information on the prices of financial products, such as stocks and other similar instruments. It is not designed to execute real transactions on the stock exchange or engage in actual financial operations. Users agree to use the Application for informational and monitoring purposes only.
+
+        2. Financial Information:
+
+        The information provided by the Application is based on sources considered reliable. However, we cannot guarantee the accuracy, completeness, or constant updating of such information. Users are responsible for verifying the accuracy of the data before making decisions based on it.
+
+        3. Limited Liability:
+
+        The use of the Application is at your own risk. We are not liable for any loss, damage, or harm, whether direct or indirect, that may arise as a result of using the Application. This includes but is not limited to data loss, service interruption, or damage to electronic devices.
+
+        4. No Financial Advice:
+
+        The Application does not provide financial advice or recommend specific transactions. Users should consult with a professional financial advisor before making decisions based on the information provided by the Application.
+
+        5. Privacy:
+
+        We respect your privacy. The Application does not collect or store personally identifiable information about users. However, the use of the Application may be subject to the Privacy Policy, available [link to privacy policy].
+
+        6. Updates and Changes:
+
+        We reserve the right to make changes to the Application, including adding or removing features, at any time and without prior notice. Users will be notified of significant changes to the terms and conditions.
+
+        7. Termination of Use:
+
+        We reserve the right to block or terminate a user's access to the Application at any time and for any reason, without prior notice.
+
+        8. Applicable Law:
+
+        These terms and conditions are governed by the laws of [country or jurisdiction], and any dispute shall be subject to the exclusive jurisdiction of the courts of [city, state, or country].
+
+        By using the Application, the user confirms that they have read, understood, and agreed to these terms and conditions. If you do not agree with any of these terms, we urge you not to use the Application.
+      </p>
+    </div>
+  )
+}
+
+export default Terms
